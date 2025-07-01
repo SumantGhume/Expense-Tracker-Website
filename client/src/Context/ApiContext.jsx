7cb75f4bff95987c9bd3f904d3a3115d641c7447
SumantGhume <116431128+SumantGhume@users.noreply.github.com>
2025-07-01 21:34:23 +0530

    Update ApiContext.jsx
    URL Updated.

--- client/src/Context/ApiContext.jsx
+++ client/src/Context/ApiContext.jsx
@@ -1,7 +1,7 @@
 import React, { createContext, useContext } from "react";
 
 // ✅ Set your base URL here (local, Railway, etc.)
-const BASE_URL = "https://expense-tracker-website-swart.vercel.app/"; // Change this in one place
+const BASE_URL = "https://expense-tracker-website-6w12.onrender.com"; // Change this in one place
 
 const ApiContext = createContext();
 
