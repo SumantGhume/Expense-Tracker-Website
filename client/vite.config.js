6895f376beb9deac88b88c870f3a7019e82855f4
suman <suman.sumantghume12@gmail.com>
2025-07-01 19:53:03 +0530

    Expense Tracker Completed

--- client/vite.config.js
+++ client/vite.config.js
@@ -3,5 +3,8 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  base: './', // 👈 ensures relative paths work in production
+  build: {
+    outDir: "dist"
+  },
+  base: "./", 
 });
